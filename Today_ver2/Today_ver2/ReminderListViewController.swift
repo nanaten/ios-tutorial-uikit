e8141786533d606ca5ec73e26fec8e4d3cd8f2e6
m.kouda <mikiko.koda@fuller.co.jp>
2022-09-14 09:26:52 +0900

    add newProject

---
+++ Today_ver2/Today_ver2/ReminderListViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Today_ver2
+//
+//  Created by Mikiko Koda on 2022/09/13.
+//
+
+import UIKit
+
+class ReminderListViewController: UICollectionViewController {
+    typealias DataSource = UICollectionViewDiffableDataSource<Int, String>
+    
+    var dataSource: DataSource!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let listLayout = listLayout()
+        collectionView.collectionViewLayout = listLayout
+        
+        let cellRegistration = UICollectionView.CellRegistration { (cell: UICollectionViewListCell, indexPath: IndexPath, itemIdentifier: String) in
+            let reminder = Reminder.sampleData[indexPath.item]
+            var contentConfiguration = cell.defaultContentConfiguration()
+            contentConfiguration.text = reminder.title
+            cell.contentConfiguration = contentConfiguration
+        }
+        dataSource = DataSource(collectionView: collectionView) { (collectionView: UICollectionView, indexPath: IndexPath, itemIdentifier: String) in
+            return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: itemIdentifier)
+        }
+    }
+
+    private func listLayout() -> UICollectionViewCompositionalLayout {
+        var listConfigration = UICollectionLayoutListConfiguration(appearance: .grouped)
+        listConfigration.showsSeparators = false
+        listConfigration.backgroundColor = .clear
+        return UICollectionViewCompositionalLayout.list(using: listConfigration)
+    }
+}
+
