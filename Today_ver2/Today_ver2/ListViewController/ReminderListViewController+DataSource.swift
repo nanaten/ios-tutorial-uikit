359d58b5395a3dc768eda4182831a5a693572475
m.coder <kurokoshow00@gmail.com>
2022-11-13 09:06:09 +0900

    progress Displaying cell info section 4 (#5)

---
+++ Today_ver2/Today_ver2/ListViewController/ReminderListViewController+DataSource.swift
@@ -0,0 +1,26 @@
+//
+//  ReminderListViewController+DataSource.swift
+//  Today_ver2
+//
+//  Created by mikiko on 2022/11/05.
+//
+
+import UIKit
+
+extension ReminderListViewController {
+    typealias DataSource = UICollectionViewDiffableDataSource<Int, String>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<Int, String>
+    
+    func cellRegistrationHandler(cell: UICollectionViewListCell, indexPath: IndexPath, id: String) {
+        let reminder = Reminder.sampleData[indexPath.item]
+        var contentConfiguration = cell.defaultContentConfiguration()
+        contentConfiguration.text = reminder.title
+        contentConfiguration.secondaryText = reminder.dueDate.dayAndTimeText
+        cell.contentConfiguration = contentConfiguration
+        
+        var backgroundConfiguration = UIBackgroundConfiguration.listGroupedCell()
+        backgroundConfiguration.backgroundColor = .todayListCellBackground
+        cell.backgroundConfiguration = backgroundConfiguration
+        
+    }
+}
---
+++ Today_ver2/Today_ver2/Models/UIColor+Today.swift
@@ -0,0 +1,15 @@
+//
+//  UIColor+Today.swift
+//  Today_ver2
+//
+//  Created by mikiko on 2022/11/13.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    class var todayListCellBackground: UIColor {
+        return UIColor(named: "ListCellBackground")!
+    }
+}
